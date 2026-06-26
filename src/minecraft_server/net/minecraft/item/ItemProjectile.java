1b2be28c63c0b14fe7c776673502f2f75addc21b
SF-24 <legonexoknights123@gmail.com>
2026-06-26 21:29:55 +0100

    Ported client wind charge code

--- src/minecraft_server/net/minecraft/item/ItemProjectile.java
+++ src/minecraft_server/net/minecraft/item/ItemProjectile.java
@@ -3,7 +3,6 @@ package net.minecraft.item;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntitySnowball;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.stats.StatList;
 import net.minecraft.world.World;
 
@@ -29,9 +28,24 @@ public class ItemProjectile extends Item
 
         worldIn.playSoundAtEntity(playerIn, "random.bow", 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
 
+        if(variant==1) playerIn.getCooldownTracker().setCooldown(this, 20);
+
         if (!worldIn.isRemote)
         {
-            worldIn.spawnEntityInWorld(new EntitySnowball(worldIn, playerIn,this.variant));
+            switch (variant) {
+                case 0:
+                    worldIn.spawnEntityInWorld(new EntitySnowball(worldIn, playerIn,this.variant));
+                    break;
+                case 1:
+                    EntitySnowball entitySnowball = new EntitySnowball(worldIn,playerIn,this.variant);
+                    double dirX = -Math.sin(Math.toRadians(playerIn.rotationYaw)) * Math.cos(Math.toRadians(playerIn.rotationPitch));
+                    double dirY = -Math.sin(Math.toRadians(playerIn.rotationPitch));
+                    double dirZ = Math.cos(Math.toRadians(playerIn.rotationYaw)) * Math.cos(Math.toRadians(playerIn.rotationPitch));
+                    entitySnowball.setThrowableHeading(dirX,dirY,dirZ, 1.8f, 1.0f);
+                    worldIn.spawnEntityInWorld(entitySnowball);
+                    // TODO: ADD A COOLDOWN.
+                    break;
+            }
         }
 
         playerIn.triggerAchievement(StatList.objectUseStats[Item.getIdFromItem(this)]);
