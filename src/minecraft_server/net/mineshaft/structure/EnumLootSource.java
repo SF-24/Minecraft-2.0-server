645d7f71b79d775277e450742f0cf19cf42e5dfb
SF-24 <legonexoknights123@gmail.com>
2026-07-13 10:41:49 +0200

    Fixed bug due to missing class and List.of() not existing in Java 8

--- src/minecraft_server/net/mineshaft/structure/EnumLootSource.java
+++ src/minecraft_server/net/mineshaft/structure/EnumLootSource.java
@@ -2,16 +2,18 @@ package net.mineshaft.structure;
 
 import net.minecraft.enchantment.Enchantment;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public enum EnumLootSource {
 
-    NETHER_FORTRESS(true, List.of(Enchantment.flame,Enchantment.fireAspect,Enchantment.fireProtection,Enchantment.smite)),
-    NETHER_BASEMENT(true, List.of()), // TODO:
-    JUNGLE_PYRAMID(false, List.of(Enchantment.baneOfArthropods,Enchantment.projectileProtection,Enchantment.punch)),
-    DESERT_PYRAMID(false, List.of(Enchantment.smite, Enchantment.blastProtection,Enchantment.knockback)),
-    STRONGHOLD(true, List.of(Enchantment.featherFalling, Enchantment.power, Enchantment.infinity)), // TODO:
-    DUNGEON(false, List.of()),;
+    NETHER_FORTRESS(true, Arrays.asList(Enchantment.flame, Enchantment.fireAspect, Enchantment.fireProtection, Enchantment.smite)),
+    NETHER_BASEMENT(true, Arrays.asList()), // TODO:
+    JUNGLE_PYRAMID(false, Arrays.asList(Enchantment.baneOfArthropods,Enchantment.projectileProtection,Enchantment.punch)),
+    DESERT_PYRAMID(false, Arrays.asList(Enchantment.smite, Enchantment.blastProtection,Enchantment.knockback)),
+    STRONGHOLD(true, Arrays.asList(Enchantment.featherFalling, Enchantment.power, Enchantment.infinity)), // TODO:
+    DUNGEON(false, Arrays.asList()),;
 
     private final List<Enchantment> nativeEnchantments;
     private final boolean allowSuperEnchants;
